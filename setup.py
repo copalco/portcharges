3a9e9747099a59553c19a9cc5a31b1be69e981d7
Piotr Kopalko <piotr.ko@webinterpret.com>
2017-03-13 23:34:20 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='portcharges',
+    version='0.0.1.dev',
+    packages=('portcharges',),
+    include_package_data=True,
+    classifiers=(
+        'Programming Language :: Python :: 3.5',
+    ),
+    install_requires=(),
+    zip_safe=False,
+)
